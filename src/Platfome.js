af89a7eb83257fa4ea49532e9cf1a759a63cecc6
AYAKA IGETA <ayaka_igeta@AYAKAnoMacBook-Pro.local>
2024-03-21 16:04:27 +0900

    ver0.1

--- src/Platfome.js
+++ src/Platfome.js
@@ -1,4 +1,4 @@
-import { Box, Center, Text, Image } from '@chakra-ui/react';
+import { Box, Center, Text, Image, Button } from '@chakra-ui/react';
 
 function App() {
   return (
@@ -20,8 +20,7 @@ function App() {
         p={{ base: "16px", md: "32px" }} // Responsive padding
       >
 
-        
-        <Center my={{ base: "20px", md: "40px" }} mb={{ base: "20px", md: "80px" }}>
+        <Center my={{ base: "20px", md: "40px" }} mb={{ base: "20px", md: "20px" }}>
           <Text
             fontSize={{ base: "24px", md: "32px" }}
             fontFamily="'Helvetica'"
@@ -31,7 +30,7 @@ function App() {
               content: "''",
               position: 'absolute',
               display: 'block',
-              height: '4px',
+              height: '8px',
               width: '100%',
               bottom: '0',
               marginBottom: '-4px',
@@ -42,7 +41,27 @@ function App() {
           </Text>
         </Center>
 
-        <Center my={{ base: "20px", md: "40px" }} mb={{ base: "20px", md: "80px" }}>
+        <Center >
+        <Image 
+          src="Ideaflowbank.png" 
+          alt="イメージの説明" 
+          boxSize={{ base: "100px", md: "800px" }} 
+          objectFit="contain" 
+        />
+      </Center>
+
+
+        <Center my={{ base: "20px", md: "40px" }} mb={{ base: "20px", md: "20px" }}>
+        <Button
+          bg="#FFC857" 
+          color="white"
+          _hover={{ bg: "#e6b347" }} // ホバー時の背景色
+        >
+          What's Idea Flow Bank?
+        </Button>
+      </Center>
+
+        <Center my={{ base: "20px", md: "40px" }} >
           <Text
             fontSize={{ base: "24px", md: "32px" }}
             fontFamily="'Helvetica'"
@@ -52,7 +71,7 @@ function App() {
               content: "''",
               position: 'absolute',
               display: 'block',
-              height: '4px',
+              height: '8px',
               width: '100%',
               bottom: '0',
               marginBottom: '-4px',
@@ -63,12 +82,10 @@ function App() {
           </Text>
         </Center>
 
-        {/* Image placeholders for screenshots */}
         <Center my={{ base: "20px", md: "40px" }}>
           <Box d="flex" justifyContent="space-around" flexWrap="wrap">
-            <Image src="/path/to/your/image1.png" alt="スクリーンショット1" p="4" />
-            <Image src="/path/to/your/image2.png" alt="スクリーンショット2" p="4" />
-            {/* Use the actual paths to your screenshot images */}
+            <Image src="homeschool.png" alt="homeschool" p="4" />
+            <Image src="homeschool.png" alt="inner journal" p="4" />
           </Box>
         </Center>
       </Box>
